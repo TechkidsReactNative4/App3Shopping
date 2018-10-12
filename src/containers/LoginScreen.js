b894e09ac886d42c69e06730c51232a237c368ae
QkLahpita <lpita.91@gmail.com>
2018-10-12 21:48:53 +0700

    Part 1: Login UI

--- src/containers/LoginScreen.js
+++ src/containers/LoginScreen.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import {
-  Text,
-  View,
+  Text, TextInput,
+  View, Platform,
   Image, StyleSheet
 } from 'react-native';
+import { Button } from 'react-native-elements';
 
-import { backgroundColor } from '../styles'
+import { backgroundColor, primaryColorBrown, primaryColorGreen, primaryColorRed } from '../styles'
 
 class LoginScreen extends Component {
   state = {}
@@ -17,10 +18,75 @@ class LoginScreen extends Component {
       source={require('../../imgs/logo_app.jpg')} />
   )
 
+  renderEmailInput = () => (
+    <View>
+      <View style={{ flexDirection: 'row' }}>
+        <Image
+          style={styles.icon}
+          resizeMode='contain'
+          source={require('../../imgs/ic_email.png')} />
+        <Text style={styles.textTitle}>Email</Text>
+      </View>
+      <TextInput
+        style={styles.input}
+        keyboardType={'email-address'}
+        underlineColorAndroid={'rgba(0,0,0,0)'} />
+    </View>
+  )
+
+  renderPasswordInput = () => (
+    <View style={{ marginTop: 20 }}>
+      <View style={{ flexDirection: 'row' }}>
+        <Image
+          style={styles.icon}
+          resizeMode='contain'
+          source={require('../../imgs/ic_password.png')} />
+        <Text style={styles.textTitle}>Password</Text>
+      </View>
+      <TextInput
+        style={styles.input}
+        underlineColorAndroid={'rgba(0,0,0,0)'}
+      />
+    </View>
+  )
+
+  renderButtons = () => (
+    <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
+      <Button
+        title='Sign up'
+        titleStyle={{ fontSize: 16 }}
+        buttonStyle={{
+          backgroundColor: primaryColorGreen,
+          width: 100,
+          height: 40,
+          borderRadius: 30
+        }}
+        containerStyle={{ marginTop: 20, marginHorizontal: 5 }} />
+      <Button
+        title='Sign in'
+        titleStyle={{ fontSize: 16 }}
+        buttonStyle={{
+          backgroundColor: primaryColorRed,
+          width: 100,
+          height: 40,
+          borderRadius: 30
+        }}
+        containerStyle={{ marginTop: 20, marginHorizontal: 5 }} />
+    </View>
+  )
+
+  renderError = () => (
+    <Text style={styles.error}>Error:...</Text>
+  )
+
   render() {
     return (
       <View style={styles.container}>
         {this.renderLogo()}
+        {this.renderEmailInput()}
+        {this.renderPasswordInput()}
+        {this.renderError()}
+        {this.renderButtons()}
       </View>
     );
   }
@@ -30,12 +96,31 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: backgroundColor,
-    justifyContent: 'center'
+    justifyContent: 'center',
+    paddingHorizontal: 30
   },
   logo: {
     width: 300,
     height: 300,
     alignSelf: 'center'
+  },
+  icon: {
+    width: 16,
+    height: 16
+  },
+  textTitle: {
+    marginStart: 5,
+    color: primaryColorBrown
+  },
+  input: {
+    borderBottomWidth: 1,
+    borderColor: primaryColorBrown,
+    paddingVertical: Platform.OS === 'ios' ? 5 : 0,
+    paddingHorizontal: 0,
+  },
+  error: {
+    color: primaryColorRed,
+    marginTop: 20
   }
 })
 
